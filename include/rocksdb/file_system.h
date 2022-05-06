bfe8dd07967b8d6dbf98f7e466753dfefea0c873
changyubi <changyubi@fb.com>
2022-05-06 11:27:24 -0700

    Use file size to set read limit in SequentialFileReader

--- include/rocksdb/file_system.h
+++ include/rocksdb/file_system.h
@@ -747,6 +747,11 @@ class FSSequentialFile {
   // open.
   virtual Temperature GetTemperature() const { return Temperature::kUnknown; }
 
+  // Get the file size. After call, size is set to the file size if
+  // ok status is returned.
+  virtual IOStatus GetFileSize(uint64_t& /* size */) const {
+    return IOStatus::NotSupported("GetFileSize");
+  }
   // If you're adding methods here, remember to add them to
   // SequentialFileWrapper too.
 };
@@ -1546,6 +1551,9 @@ class FSSequentialFileWrapper : public FSSequentialFile {
   }
   Temperature GetTemperature() const override {
     return target_->GetTemperature();
+  }
+  IOStatus GetFileSize(uint64_t& size) const override {
+    return target_->GetFileSize(size);
   }
 
  private:
